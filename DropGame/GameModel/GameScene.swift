49b8c9de7c1fc72e2955618814c264ef2eed35c8
jclegg03 <jjc2003@comcast.net>
2021-11-15 12:39:37 -0700

    Started the colorMask

--- DropGame/GameModel/GameScene.swift
+++ DropGame/GameModel/GameScene.swift
@@ -8,18 +8,21 @@
 import Foundation
 import SpriteKit
 
-class GameScene : SKScene
+class GameScene : SKScene , SKPhysicsContactDelegate
 {
+    private var colorMask : Int = 0b0000
+    
     override func didMove(to view : SKView) -> Void
     {
         physicsBody = SKPhysicsBody(edgeLoopFrom : frame)
+        physicsWorld.contactDelegate = self
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) -> Void
     {
         guard let touch = touches.first
         else { return }
-        let currentColor = UIColor.purple
+        let currentColor = assignColorAndBitmask()
         let width = Int(arc4random() % 50)
         let height = Int(arc4random() % 50)
         let location = touch.location(in: self)
@@ -31,5 +34,15 @@ class GameScene : SKScene
         node.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: width, height: height))
         
         addChild(node)
+    }
+    
+    private func assignColorAndBitmask() -> UIColor
+    {
+        let colors : [UIColor] = [.purple, .red, .blue, .green, .darkGray, .cyan, .brown, .yellow]
+        let randomIndex = Int(arc4random()) % colors.count
+        
+        colorMask = randomIndex + 1
+        
+        return colors[randomIndex]
     }
 }
