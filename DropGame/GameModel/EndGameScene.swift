e38bd9785f6f053789b94dac3dc53fd26d3bf035
jclegg03 <jjc2003@comcast.net>
2021-11-19 11:45:26 -0700

    Added the end game file

---
+++ DropGame/GameModel/EndGameScene.swift
@@ -0,0 +1,39 @@
+//
+//  EndGameScene.swift
+//  DropGame
+//
+//  Created by Clegg, Jay on 11/19/21.
+//
+
+import Foundation
+import SpriteKit
+
+class EndGameScene : SKScene
+{
+    var gameScore : Int = 0
+    
+    override public func didMove(to view : SKView) -> Void
+    {
+        backgroundColor = .blue
+        
+        let scoreNode : SKLabelNode = SKLabelNode(fontNamed: "Copperplate-Bold")
+        let endNode : SKLabelNode = SKLabelNode(fontNamed: "Copperplate-Bold")
+        let restartNode : SKLabelNode = SKLabelNode(fontNamed: "Copperplate-Bold")
+        
+        scoreNode.position.x = frame.midX
+        scoreNode.position.y = frame.midY + 30
+        scoreNode.text = "Score: \(gameScore)"
+        
+        endNode.position.x = frame.midX
+        endNode.position.y = frame.midY + 10
+        endNode.text = "Game Over!"
+        
+        restartNode.position.x = frame.midX
+        restartNode.position.y = frame.midY - 10
+        restartNode.text = "Pinch to restart"
+        
+        addChild(scoreNode)
+        addChild(endNode)
+        addChild(restartNode)
+    }
+}
